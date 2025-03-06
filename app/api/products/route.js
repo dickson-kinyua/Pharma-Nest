40e2a747396b8bd4beaacc41099b7f8731324ae4
Dickson <dicksonkinyua@gmail.com>
2025-03-06 11:51:47 +0300

    debugging

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -48,6 +48,9 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+console.log(process.env.CLOUDINARY_API_SECRET);
 
 const uploadToCloudinary = async (buffer, fileName) => {
   return new Promise((resolve, reject) => {
